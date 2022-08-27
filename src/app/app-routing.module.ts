54b9c638cca9bc1ed074bff17a64cf9c33718d2a
Vittor Daniel <vittordaniel1108@gmail.com>
2022-08-27 01:31:19 -0300

    inital Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LostPasswordComponent } from './pages/login/lost-password/lost-password.component';
+import { NewUserComponent } from './pages/login/new-user/new-user.component';
+import { SignComponent } from './pages/login/sign/sign.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {
+    path: 'login',
+    component: LoginComponent,
+    children:[
+      {path: 'login', component: LoginComponent},
+      {path: 'lost-password', component: LostPasswordComponent},
+      {path: 'new-user', component: NewUserComponent},
+      {path: 'sign', component: SignComponent},
+
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LostPasswordComponent } from './pages/login/lost-password/lost-password.component';
+import { NewUserComponent } from './pages/login/new-user/new-user.component';
+import { SignComponent } from './pages/login/sign/sign.component';
+import { NameInputComponent } from './components/form/name-input/name-input.component';
+import { EmailInputComponent } from './components/form/email-input/email-input.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    HomeComponent,
+    LoginComponent,
+    LostPasswordComponent,
+    NewUserComponent,
+    SignComponent,
+    NameInputComponent,
+    EmailInputComponent,
   ],
   imports: [
     BrowserModule,
